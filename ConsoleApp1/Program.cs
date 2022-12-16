129e2f1bd1cab3f5430f398c8ffa97b1f2dce5b7
Aesop <Aesop@ATLAS>
2022-12-15 19:56:18 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Task
+{
+    public string Description { get; set; }
+    public int Priority { get; set; }
+    public DateTime DueDate { get; set; }
+}
+
+List<Task> tasks = new List<Task>();
+
