3dcbfc7cb6f5732ac7e7b6bea92de9c14a3b1497
Aesop <Aesop@ATLAS>
2022-12-15 20:10:08 -0500

    First try

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,62 @@
-﻿public class Task
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListApp
 {
-    public string Description { get; set; }
-    public int Priority { get; set; }
-    public DateTime DueDate { get; set; }
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> toDoList = new List<string>();
 
-List<Task> tasks = new List<Task>();
 
+            while (true)
+            {
+                ShowMenu();
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Enter the to-do itme: ");
+                    string toDoItem = Console.ReadLine();
+                    AddItem(toDoList, toDoItem);
+                }
+                else if (choice == "2")
+                {
+                    Console.Write("Enter the index of the to-do item to delete:");
+                    int index = int.Parse(Console.ReadLine());
+                    DeleteItem(toDoList, index);
+                }
+                else if (choice == "3")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.")
+                }
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("To-Do List App");
+            Console.WriteLine("1. Add a to-do item");
+            Console.WriteLine("2. Delete a to-do item");
+            Console.WriteLine("3. Quit");
+        }
+
+        static void AddItem(List<string> toDoList, string item)
+        {
+            toDoList.Add(item);
+            Console.WriteLine("Item added.");
+            ShowToDoList(toDoList);
+        }
+
+        static void DeleteItem(List<string> toDoList, int index)
+        {
+            toDoList.RemoveAt(index);
+            Console.WriteLine("Item deleted.");
+            ShowToDoList(toDoList);
+        }
